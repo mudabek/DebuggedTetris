c4f36ce329fc7b1cf3a42087dbcb1bc0d08ed6f6
Connor L. Gregorich-Trevor <gregoric@cs.grinnell.edu>
2016-09-18 15:28:22 -0500

    Bug 6 of 5: fixed row delete

--- src/com/gamewerks/blocky/engine/Board.java
+++ src/com/gamewerks/blocky/engine/Board.java
@@ -54,14 +54,15 @@ public class Board {
         }
     }
     
+    //fixed this so the rows get deleted properly
     public void deleteRow(int n) {
-        for (int row = 0; row < n - 1; row++) {
-            for (int col = 0; col < Constants.BOARD_WIDTH; col++) {
-                well[row][col] = well[row+1][col];
-            }
-        }
-        for (int col = 0; col < Constants.BOARD_WIDTH; col++) {
+    	for (int col = 0; col < Constants.BOARD_WIDTH; col++) {
             well[n][col] = false;
+        }
+    	for (int row = n - 1; row > 0; row--) {
+            for (int col = 0; col < Constants.BOARD_WIDTH; col++) {
+                well[row + 1][col] = well[row][col];
+            }
         }
     }
     
@@ -84,7 +85,8 @@ public class Board {
         List completedRows = new LinkedList();
         for (int row = 0; row < Constants.BOARD_HEIGHT; row++) {
             if (isCompletedRow(row)) {
-                completedRows.add(well[row]);
+            	//changed well(row) to row (got help from Linh)
+                completedRows.add(row);
             }
         }
         return completedRows;
